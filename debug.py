5155c35c8996e64843d627715cb60ba1d4d88005
Olivier Vitrac <olivier.vitrac@agroparistech.fr>
2023-08-12 01:03:33 +0200

    my own test file used for debugging (change spacefilling from False to True to see differences)

--- debug.py
+++ debug.py
@@ -8,10 +8,22 @@ Created on Thu Jul 27 10:56:21 2023
 
 from pizza.region import *
 
+
+# --- do not look at this code, it is for me (Olivier)
+# a=LammpsSpacefilling(style="$block",width=100,height=100,depth=100,units="",beadtype=1)
+# inputs = a.DEFINITIONS+a.USER
+# inputs.formateval("${width}=${style}+3")
+# print(a.do)
+
+
+# check here if you want a spacefilling model or not
+spacefilling = True
+beadtype_forglob = 2 if spacefilling else 1
+
 mag = 1
 e = emulsion(xmin=-5*mag, ymin=-5*mag, zmin=-5*mag,xmax=5*mag, ymax=5*mag, zmax=5*mag)
-e.insertion([2,2,2],beadtype=1)
-C = region(name='debug',width=11*mag,height=11*mag,depth=11*mag)
+e.insertion([2,2,2],beadtype=beadtype_forglob)
+C = region(name='debug',width=11*mag,height=11*mag,depth=11*mag,spacefilling=spacefilling)
 C.scatter(e)
 C.emulsion.glob00.creategroup()
 C.emulsion.creategroup()
